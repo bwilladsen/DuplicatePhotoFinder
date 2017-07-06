c83d284ad09965ef61dd72aa699d4c5d6832a2e1
Brad Willadsen <bwilladsen@gmail.com>
2017-07-06 15:11:46 -0400

    Initial implementation of unit tests

---
+++ DuplicatePhotoFinderTests/Utilities/ExtensionMethodsTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DuplicatePhotoFinder.Tests
+{
+    [TestClass()]
+    public class ExtensionMethodsTests
+    {
+        [TestMethod()]
+        public void RootPathShouldBeValidPath()
+        {
+            // Arrange
+            String path = @"D:\";
+            bool isValid = false;
+
+            // Act
+            isValid = path.IsValidPath();
+
+            // Assert
+            Assert.IsTrue(isValid, "Root path is not a valid path when it should be.");
+        }
+
+        [TestMethod()]
+        public void PathWithFolderNameShouldBeValidPath()
+        {
+            // Arrange
+            String path = @"D:\TestFolder";
+            bool isValid = false;
+
+            // Act
+            isValid = path.IsValidPath();
+
+            // Assert
+            Assert.IsTrue(isValid, "Path with folder name is not a valid path when it should be.");
+        }
+
+        [TestMethod()]
+        public void PathWithInvalidCharactersShouldBeInvalidPath()
+        {
+            // Arrange
+            String path = @"D:/TestFolder";
+            bool isValid = false;
+
+            // Act
+            isValid = path.IsValidPath();
+
+            // Assert
+            Assert.IsFalse(isValid, "Path with invalid characters should not be a valid path.");
+        }
+
+        [TestMethod()]
+        public void UNCPathShouldBeValidPath()
+        {
+            // Arrange
+            String path = @"\\server\share";
+            bool isValid = false;
+
+            // Act
+            isValid = path.IsValidPath();
+
+            // Assert
+            Assert.IsTrue(isValid, "UNC Path is not a valid path when it should be.");
+        }
+    }
+}
