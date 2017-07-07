85426c0ed0edbcce3be88bf90755b6b413bd3186
Brad Willadsen <bwilladsen@gmail.com>
2017-07-07 14:47:11 -0400

    Search for duplicate pictures, and identify duplicates

--- DuplicatePhotoFinder/ViewModel/PhotoFinderViewModel.cs
+++ DuplicatePhotoFinder/ViewModel/PhotoFinderViewModel.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Windows.Media.Imaging;
 
 namespace DuplicatePhotoFinder
 {
     public class PhotoFinderViewModel : BaseViewModel
     {
+        #region Variables
+
+        private Thread searchThread = null;
+        Dictionary<String, List<PhotoViewModel>> photos = new Dictionary<String, List<PhotoViewModel>>();
+        List<String> duplicatePhotos = new List<String>();
+
+        #endregion
+
         #region Commands
 
         private UICommand searchCommand;
@@ -27,6 +39,7 @@ namespace DuplicatePhotoFinder
         private void searchExecuteCommandHandler(object sender)
         {
             IsSearching = true;
+            startSearching();
         }
 
         #endregion
@@ -77,9 +90,71 @@ namespace DuplicatePhotoFinder
 
         #region Searching
 
-        private void searchForDuplicatePictures()
+        private void startSearching()
         {
+            searchThread = new Thread(() => searchForDuplicatePictures(PicturePath));
+            searchThread.IsBackground = true;
+            searchThread.Name = "Picture Search Thread";
+            searchThread.Start();
+        }
 
+        private void searchForDuplicatePictures(String path)
+        {
+            try
+            {
+                DirectoryInfo currentDirectory = new DirectoryInfo(path);
+                foreach (FileInfo currentFile in currentDirectory.GetFiles("*.jpg"))
+                {
+                    DateTime dateTaken = getDateTaken(currentFile.FullName);
+                    PhotoViewModel photoViewModel = new PhotoViewModel();
+                    photoViewModel.DateTaken = dateTaken;
+                    photoViewModel.Path = currentFile.FullName;
+                    photoViewModel.Size = currentFile.Length;
+                    String hash = photoViewModel.ToString();
+                    
+                    if (!photos.ContainsKey(hash))
+                    {
+                        photos[hash] = new List<PhotoViewModel>();
+                    }
+                    else
+                    {
+                        if(!duplicatePhotos.Contains(hash))
+                        {
+                            duplicatePhotos.Add(hash);
+                        }
+                    }
+                    photos[hash].Add(photoViewModel);
+                }
+
+                foreach (DirectoryInfo subDirectory in currentDirectory.GetDirectories())
+                {
+                    searchForDuplicatePictures(subDirectory.FullName);
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO:  Logging
+            }
+
+        }
+
+        private DateTime getDateTaken(string inFullPath)
+        {
+            DateTime returnDateTime = DateTime.MinValue;
+            try
+            {
+                using (FileStream picStream = new FileStream(inFullPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    BitmapSource bitSource = BitmapFrame.Create(picStream);
+                    BitmapMetadata metaData = bitSource.Metadata as BitmapMetadata;
+                    returnDateTime = DateTime.Parse(metaData.DateTaken);
+                }
+            }
+            catch
+            {
+                // TODO:  Logging of some sorts  
+            }
+            return returnDateTime;
         }
 
         #endregion
---
+++ DuplicatePhotoFinder/ViewModel/PhotoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicatePhotoFinder
+{
+    public class PhotoViewModel : BaseViewModel
+    {
+        public String Path { get; set; }
+        public DateTime DateTaken { get; set; }
+        public long Size { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0}-{1}", DateTaken.Ticks, Size);
+        }
+        
+    }
+}
