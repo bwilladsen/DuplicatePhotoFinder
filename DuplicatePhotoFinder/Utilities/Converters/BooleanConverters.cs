a090ac04ce6218f76d1a0dd4f7189be145bf1df2
Brad Willadsen <bwilladsen@gmail.com>
2017-07-14 12:46:53 -0400

    Disable the path field while searching

---
+++ DuplicatePhotoFinder/Utilities/Converters/BooleanConverters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DuplicatePhotoFinder
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public sealed class OppositeBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+                return null;
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (Equals(value, false))
+                return true;
+            if (Equals(value, true))
+                return false;
+            return null;
+        }
+    }
+}
